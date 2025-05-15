b4846c3247c33109ab869078d277932f2284b039
lbmalinao21 <lbmalinao.student@asiancollege.edu.ph>
2025-05-15 21:51:52 +0800

    Delete GUIPRO-final-project-main/GUIPRO-final-project-main directory

--- GUIPRO-final-project-main/GUIPRO-final-project-main/lib/models/product.dart
+++
@@ -1,8 +0,0 @@
-class Product {
-  final String name;
-  final double price;
-  final String image;
-  final String category;
-
-  Product({required this.name, required this.price, required this.image, required this.category});
-}
